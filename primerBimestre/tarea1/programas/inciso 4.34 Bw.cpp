69ee0347aae79366248bc3fc31ea134dd39236f3
William Mayen <willysomv101@gmail.com>
2022-02-16 22:26:15 -0600

    inciso4.34B

---
+++ primerBimestre/tarea1/programas/inciso 4.34 Bw.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int fact(int);
+int main()
+{
+    unsigned int s;
+    cout<<"Escriba un numero:";
+    cin>>s;
+    double e=0;
+    for(unsigned int i=0; i<s;i++){
+        double u=fact(i);
+        e+=1/u;
+    }
+    cout<<"valor aproximado a e""  "<<s<<"    " "es""   "<<e<<endl;
+}
+int fact (int f){
+    int factorial=f;
+    if (factorial>1){
+        factorial=f+fact(f-1);
+        return factorial;
+    }
+    else{
+        return 1;
+    }
+}
