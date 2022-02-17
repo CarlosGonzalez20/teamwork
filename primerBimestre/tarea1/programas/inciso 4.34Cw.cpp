88a56f2626d915c59b32868408c0049613741e6a
William Mayen <willysomv101@gmail.com>
2022-02-16 22:34:30 -0600

    inciso4.34C

---
+++ primerBimestre/tarea1/programas/inciso 4.34Cw.cpp
@@ -0,0 +1,11 @@
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"programa que calcule e^x, escriba el valor de x: ";
+    cin>>x;
+    cout<<"e^"<<x<<" = "<<exp(x)<<endl;
+
+}
