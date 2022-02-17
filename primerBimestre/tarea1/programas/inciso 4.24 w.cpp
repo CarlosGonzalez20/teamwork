6d3ef3a3041994f200f9e7dd686815aa3a7534bf
William Mayen <willysomv101@gmail.com>
2022-02-16 21:04:40 -0600

    4.24errorw

---
+++ primerBimestre/tarea1/programas/inciso 4.24 w.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"Inciso A"<<endl;
+if (y == 8)
+if (x == 5)
+cout<<"@@@@@"<<endl;
+else
+cout<<"#####"<<endl;
+cout<<"$$$$$"<<endl;
+cout<<"&&&&&"<<endl;
+
+cout<<"Inciso B"<<endl;
+if (y==8){
+if (x == 5)
+cout<<"@@@@@"<<endl;
+}
+else{
+cout<<"#####"<<endl;
+cout<<"$$$$$"<<endl;
+cout<<"&&&&&"<<endl;
+}
+
+cout<<"Inciso C"<<endl;
+if (y == 8){
+if (x == 5)
+cout<<"@@@@@" <<endl;
+}
+else{
+cout<<"#####"<<endl;
+cout<<"$$$$$"<<endl;
+}
+cout<<"&&&&&"<<endl;
+
+cout<<"Inciso D"<<endl;
+if (y ==8){
+if (x==5)
+cout<<"@@@@@"endl;
+}
+else{
+cout<<"#####"<<endl;
+cout<<"$$$$$"<<endl;
+cout<<"&&&&&"<<endl;
+}
+
+}
+
+
+
+
