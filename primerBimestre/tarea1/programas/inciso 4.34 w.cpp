600d9a7abc8464edf2cf76e0c1c7cdde73d02706
William Mayen <willysomv101@gmail.com>
2022-02-16 22:05:07 -0600

    4.35incisoA

---
+++ primerBimestre/tarea1/programas/inciso 4.34 w.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, i, factorial;
+
+    cout<<"Introduce un numero";
+    cin>> n;
+    factorial=1;
+    for(i=1; i<=n; i++)
+    {
+
+
+        factorial=factorial*i;
+    }
+    cout<<"Factorial de "<<n<<"->"<<factorial<<endl;
+
+
+}
