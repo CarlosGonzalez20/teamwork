1f8fbd703f964e996adf4bbb593c42e0698293ff
CarlosGonzalez20 <ll3224645@gmail.com>
2022-03-21 18:50:01 -0600

    adding homework (encabezados e implementacion)

--- segundoBimestre/encabezados/tareaEncabezados/main.cpp
+++ segundoBimestre/encabezados/tareaEncabezados/main.cpp
@@ -1,10 +1,13 @@
-#include<iostream>
+#include <iostream>
+#include "menu.h"
+using namespace std;
+
 #include<fstream>
 #include<stdlib.h>
 #include<cstdlib>
 #include<conio.h>
 #include<iomanip>
-using namespace std;
+
 class persona
 {
 	private:
@@ -17,60 +20,7 @@ class persona
 		void search();
 		void delet();
 };
-void persona::menu()
-{
-    int choice;
-	char x;
-	do
-    {
-	system("cls");
 
-	cout<<"\t\t\t-------------------------------"<<endl;
-	cout<<"\t\t\t |   SISTEMA GESTION PERSONAS  |"<<endl;
-	cout<<"\t\t\t-------------------------------"<<endl;
-	cout<<"\t\t\t 1. Ingreso Personas"<<endl;
-	cout<<"\t\t\t 2. Despliegue Personas"<<endl;
-	cout<<"\t\t\t 3. Modifica Personas"<<endl;
-	cout<<"\t\t\t 4. Busca Personas"<<endl;
-	cout<<"\t\t\t 5. Borra Personas"<<endl;
-	cout<<"\t\t\t 6. Exit"<<endl;
-
-	cout<<"\t\t\t-------------------------------"<<endl;
-	cout<<"\t\t\tOpcion a escoger:[1/2/3/4/5/6]"<<endl;
-	cout<<"\t\t\t-------------------------------"<<endl;
-	cout<<"Ingresa tu Opcion: ";
-    cin>>choice;
-
-    switch(choice)
-    {
-    case 1:
-    	do
-    	{
-    		insert();
-    		cout<<"\n\t\t\t Agrega otra persona(Y,N): ";
-    		cin>>x;
-		}while(x=='y'||x=='Y');
-		break;
-	case 2:
-		display();
-		break;
-	case 3:
-		modify();
-		break;
-	case 4:
-		search();
-		break;
-	case 5:
-		delet();
-		break;
-	case 6:
-		exit(0);
-	default:
-		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
-	}
-	getch();
-    }while(choice!= 6);
-}
 void persona::insert()
 {
 	system("cls");
@@ -255,8 +205,7 @@ void persona::delet()
 main()
 {
 	persona estudiante;
+	menu menuPrincipal;
 	estudiante.menu();
 	return 0;
 }
-
-
---
+++ segundoBimestre/encabezados/tareaEncabezados/src/menu.cpp
@@ -0,0 +1,68 @@
+#include "menu.h"
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<cstdlib>
+#include<conio.h>
+#include<iomanip>
+
+menu::menu()
+{
+    int choice;
+	char x;
+	do
+    {
+	system("cls");
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"\t\t\t |   SISTEMA GESTION PERSONAS  |"<<endl;
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"\t\t\t 1. Ingreso Personas"<<endl;
+	cout<<"\t\t\t 2. Despliegue Personas"<<endl;
+	cout<<"\t\t\t 3. Modifica Personas"<<endl;
+	cout<<"\t\t\t 4. Busca Personas"<<endl;
+	cout<<"\t\t\t 5. Borra Personas"<<endl;
+	cout<<"\t\t\t 6. Exit"<<endl;
+
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"\t\t\tOpcion a escoger:[1/2/3/4/5/6]"<<endl;
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"Ingresa tu Opcion: ";
+    cin>>choice;
+
+    switch(choice)
+    {
+    case 1:
+    	do
+    	{
+    		insert();
+    		cout<<"\n\t\t\t Agrega otra persona(Y,N): ";
+    		cin>>x;
+		}while(x=='y'||x=='Y');
+		break;
+	case 2:
+		display();
+		break;
+	case 3:
+		modify();
+		break;
+	case 4:
+		search();
+		break;
+	case 5:
+		delet();
+		break;
+	case 6:
+		exit(0);
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+	}
+	getch();
+    }while(choice!= 6);
+
+    //ctor
+}
+
+menu::~menu()
+{
+    //dtor
+}
